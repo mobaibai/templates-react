c0c75d7b05af04d981de3edbd0cbb3011832bc96
mobaibai <puresilent.com@gmail.com>
2022-08-28 18:58:00 +0800

    🎉 分支初始化

---
+++ page2/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+import './index.scss'
+
+export default function {{name}}(){
+  return (
+    <div className="{{name}}-container">
+      <span>{{name}}Container</span>
+    </div>
+  )
+}
